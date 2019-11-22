01a426acfc9e65bd096086414b0306c8348f22c5
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2019-11-22 08:21:25 +0000

    Update Polysome_scatter_plots.R

--- R_scripts/Polysome_scatter_plots.R
+++ R_scripts/Polysome_scatter_plots.R
@@ -1,11 +1,17 @@
-###Imports
+###This script was written by Joseph A.Waldron and produces panels 2A and 3A in Waldron et al. (2019) Genome Biology
+###Input data can be downloaded from the Gene Expression Omnibus (GEO) database accessions GSE134888 which can be found at 
+###https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE134888
+
+#Imports
 library(tidyverse)
 
-#import variables----
-source("N:\\JWALDRON/R_scripts/structure_seq_variables.R")
+#set home directory----
+home <- '' #this needs to be set to the directory containing the data
 
-#set home and working directory----
-setwd('N:\\JWALDRON/Structure_seq/Paper/Figures/R/Polysomes/panels')
+#set variables----
+#posterior probability thresholds
+positive_change <- 0.25
+no_change <- 0.02
 
 #make theme----
 myTheme <- theme_bw()+
@@ -14,9 +20,14 @@ myTheme <- theme_bw()+
         legend.title = element_blank(),
         legend.text = element_text(size=18))
 
-#read in translation data
-translation_data <- read_tsv(file = file.path(home, 'raw_data/polysomes/penn-DOD-gene.mmdiffMCF7'), col_names = T, skip = 1)
+#read in translation data----
+translation_data <- read_tsv(file = file.path(home, 'penn-DOD-gene.mmdiffMCF7'), col_names = T, skip = 1)
 
+#4A-dep logFC plot----
+#assign each gene as 4A-dep, 4A-indep, 4A-antidep and NA based on posterior probability and DOD
+#eta1_1 = logFC in polysomes
+#eta1_2 = logFC in sub-polysomes
+#DOD = eta1_1 - eta1_2
 translation_data %>%
   mutate(DOD = eta1_1 - eta1_2,
          translation = factor(case_when(posterior_probability > positive_change & DOD < 0 ~ "4A-dep",
@@ -33,25 +44,7 @@ translation_data %>%
   rename(polys_logFC = eta1_1,
          subs_logFC = eta1_2) -> logFC_plot_data
 
-
-translation_data %>%
-  mutate(polysomes = rowMeans(cbind(mu_PD1_MCF7.gene, mu_PD2_MCF7.gene, mu_PD3_MCF7.gene)),
-         sub_polysomes = rowMeans(cbind(mu_SD1_MCF7.gene, mu_SD2_MCF7.gene, mu_SD3_MCF7.gene)),
-         TE_1 = mu_PD1_MCF7.gene - mu_SD1_MCF7.gene,
-         TE_2 = mu_PD1_MCF7.gene - mu_SD2_MCF7.gene,
-         TE_3 = mu_PD1_MCF7.gene - mu_SD3_MCF7.gene,
-         mean_TE = rowMeans(cbind(TE_1, TE_2, TE_3)),
-         TE = factor(case_when(mean_TE > quantile(mean_TE, 0.8) ~ "high_TE",
-                        mean_TE < quantile(mean_TE, 0.2) ~ "low_TE",
-                        mean_TE >= quantile(mean_TE, 0.2) & mean_TE <= quantile(mean_TE, 0.8) ~ "NA"),
-                     levels = c("low_TE", "high_TE", "NA"),
-                     labels = c("low TE", "high TE", "not assigned"), ordered = T),
-         alpha_score = case_when(mean_TE > quantile(mean_TE, 0.8) ~ 1,
-                                 mean_TE < quantile(mean_TE, 0.2) ~ 1,
-                                 mean_TE >= quantile(mean_TE, 0.2) & mean_TE <= quantile(mean_TE, 0.8) ~ 0.5)) %>%
-  select(feature_id, polysomes, sub_polysomes, TE, alpha_score) -> TE_plot_data
-
-#plot scatters
+#plot scatter
 logFC_scatter <- ggplot(data = logFC_plot_data, aes(x = subs_logFC, y = polys_logFC, colour = translation, alpha = alpha_score))+
   geom_point(size = 1)+
   scale_colour_manual(values=c("#74add1", "#fdae61", "#A468D5", "grey"))+
@@ -69,10 +62,26 @@ pdf(file = "subs_vs_polys_logFC.pdf", width = 6.5, height = 5)
 print(logFC_scatter)
 dev.off()
 
-TE_scatter <- ggplot(data = TE_plot_data, aes(x = sub_polysomes, y = polysomes, colour = TE, alpha = alpha_score))+
+#TE plot----
+#assign each gene as high_TE, low_TE or NA based on whether it is in the top, bottom or middle third respectively for the ratio of polysomal RNA over sub-polysomal RNA
+translation_data %>%
+  mutate(polysomes = rowMeans(cbind(mu_PD1_MCF7.gene, mu_PD2_MCF7.gene, mu_PD3_MCF7.gene)), #calculates the mean of the normalised log values for polysomal RNA
+         sub_polysomes = rowMeans(cbind(mu_SD1_MCF7.gene, mu_SD2_MCF7.gene, mu_SD3_MCF7.gene)), #calculates the mean of the normalised log values for sub-polysomal RNA
+         TE_1 = mu_PD1_MCF7.gene - mu_SD1_MCF7.gene, #subtracts normalised log values for sub-polysomal RNA from polysomal RNA in sample 1
+         TE_2 = mu_PD2_MCF7.gene - mu_SD2_MCF7.gene, #subtracts normalised log values for sub-polysomal RNA from polysomal RNA in sample 2
+         TE_3 = mu_PD3_MCF7.gene - mu_SD3_MCF7.gene, #subtracts normalised log values for sub-polysomal RNA from polysomal RNA in sample 3
+         mean_TE = rowMeans(cbind(TE_1, TE_2, TE_3)), #calculates the mean of TE_1-3
+         TE = factor(case_when(mean_TE > quantile(mean_TE, 2/3) ~ "high_TE",
+                               mean_TE < quantile(mean_TE, 1/3) ~ "low_TE",
+                               mean_TE >= quantile(mean_TE, 1/3) & mean_TE <= quantile(mean_TE, 2/3) ~ "NA"),
+                     levels = c("low_TE", "high_TE", "NA"),
+                     labels = c("low TE", "high TE", "not assigned"), ordered = T)) %>%
+  select(feature_id, polysomes, sub_polysomes, TE) -> TE_plot_data
+
+#plot scatter
+TE_scatter <- ggplot(data = TE_plot_data, aes(x = sub_polysomes, y = polysomes, colour = TE))+
   geom_point(size = 1)+
   scale_colour_manual(values=c("#7C71D8", "#FFE073", "grey20"))+
-  scale_alpha(guide = F)+
   geom_abline(lty=2)+
   geom_hline(yintercept = 0, lty=2)+
   geom_vline(xintercept = 0, lty=2)+
