660a0cc9a7ea07c75b9b1f48c0d794a9b17be474
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2020-01-15 10:18:52 +0000

    Update Specificity_and_ligation_bias.R

--- R_scripts/Specificity_and_ligation_bias.R
+++ R_scripts/Specificity_and_ligation_bias.R
@@ -1,4 +1,4 @@
-###This script was written by Joseph A. Waldron and produces panels S1F-G in Waldron et al. (2020) Genome Biology
+###This script was written by Joseph A. Waldron and produces panels S1F-G in Waldron et al. (2019) Genome Biology
 ###Input data first needs to be generated using the Shell scripts from this repository (see README file)
 
 #Imports----
